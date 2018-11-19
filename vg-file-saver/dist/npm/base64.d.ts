7bb3c624432238edac02288882249abb0c756094
vg-App <44997038+vg-App@users.noreply.github.com>
2018-11-19 11:59:19 +0200

    Add files via upload

---
+++ vg-file-saver/dist/npm/base64.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Encodes the provided string as Base64-encoded data.
+ *
+ * @param {string} plainText - The plain text to encode. Might contain UTF-8 symbols.
+ * @returns {string} - The Base64-encoded string.
+ *
+ */
+export function encodeBase64(plainText: string): string
